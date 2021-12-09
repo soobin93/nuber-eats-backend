fdc678cf4935213b52c9f7e2f1d1456c7a873bcf
Soobin <ssbin93@gmail.com>
2021-12-09 21:57:27 +1000

    Inject a repository for the restaurant entity (data mapper way)

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsResolver } from './restaurants.resolver';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantsService } from './restaurants.service';
 
 @Module({
-  providers: [RestaurantsResolver],
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+  providers: [RestaurantsResolver, RestaurantsService],
 })
 export class RestaurantsModule {}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,15 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantsService } from './restaurants.service';
 
 @Resolver(() => Restaurant)
 export class RestaurantsResolver {
+  constructor(private readonly restaurantsService: RestaurantsService) {}
+
   @Query(() => [Restaurant])
-  restaurants(@Args('isVegan') isVegan: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantsService.getAll();
   }
 
   @Mutation(() => Boolean)
