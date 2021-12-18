2ed0966a8134ca648d9ec09cff3b30c4afa87d34
Soobin <ssbin93@gmail.com>
2021-12-18 22:40:17 +1000

    Create User Model

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly users: Repository<User>,
+  ) {}
+}
